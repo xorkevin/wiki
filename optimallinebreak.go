70922593ed7c6173290f2693ce3d6d0ccf033fe8
Kevin Wang <wangkevin448@gmail.com>
2018-02-12 10:25:11 -0800

    Change optimallinebreak to iterate in reverse

--- optimallinebreak.go
+++ optimallinebreak.go
@@ -9,25 +9,22 @@ import (
 const (
 	paragraphInput  = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam suscipit diam fringilla tortor tempor bibendum. Fusce tellus libero, congue non elementum et, tristique a ante. Donec vel lectus pulvinar, vulputate ligula quis, venenatis leo. Nunc blandit a lacus id consectetur. Duis vitae elementum urna. Fusce hendrerit diam vitae feugiat aliquam. Nulla convallis id felis a pretium. Vestibulum at convallis magna. Donec ut odio mi. Curabitur malesuada purus mauris, vel ornare quam vulputate eget. Quisque vel nisi tristique, iaculis ante nec, volutpat neque. Fusce ligula diam, luctus id odio nec, ultricies mollis nunc. Ut bibendum lorem nulla, at mollis tortor semper a. Sed tempus nisl eu fringilla molestie. Praesent suscipit risus enim, ac ornare augue mollis semper.`
 	paragraphInput2 = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam suscipit diam fringilla tortor tempor bibendum. Fusce tellus libero, congue non elementum et, tristique a ante. Donec vel lectus pulvinar, vulputate ligula quis, venenatis leo. 0123456789112345678921234567893123456789 Nunc blandit a lacus id consectetur. Duis vitae elementum urna. Fusce hendrerit diam vitae feugiat aliquam. Nulla convallis id felis a pretium. Vestibulum at convallis magna. Donec ut odio mi. Curabitur malesuada purus mauris, vel ornare quam vulputate eget. Quisque vel nisi tristique, iaculis ante nec, volutpat neque. Fusce ligula diam, luctus id odio nec, ultricies mollis nunc. Ut bibendum lorem nulla, at mollis tortor semper a. Sed tempus nisl eu fringilla molestie. Praesent suscipit risus enim, ac ornare augue mollis semper.`
-
-	lineWrap = 32
 )
 
 func main() {
-	{
-		words := strings.Split(paragraphInput, " ")
-		cost, formated := dplinebreak(words, lineWrap)
-		fmt.Println("cost:", cost)
-		fmt.Println(charRuler(lineWrap))
-		fmt.Println(formated)
-	}
-	{
-		words := strings.Split(paragraphInput2, " ")
-		cost, formated := dplinebreak(words, lineWrap)
-		fmt.Println("cost:", cost)
-		fmt.Println(charRuler(lineWrap))
-		fmt.Println(formated)
-	}
+	testFormat(paragraphInput, 32)
+	testFormat(paragraphInput2, 32)
+	testFormat(paragraphInput, 80)
+	testFormat(paragraphInput2, 80)
+}
+
+func testFormat(paragraph string, lineWrap int) {
+	words := strings.Split(paragraph, " ")
+	cost, formated := dplinebreak(words, lineWrap)
+	fmt.Println("cost:", cost)
+	fmt.Println(charRuler(lineWrap))
+	fmt.Println(formated)
+	fmt.Println()
 }
 
 func dplinebreak(words []string, width int) (int, string) {
@@ -56,13 +53,15 @@ func dpCalcPartition(wordLengths []int, width int) (int, []int) {
 	for i := 1; i < l+1; i++ {
 		minCost := -1
 		minPrev := 0
-		for j := 0; j < i; j++ {
-			c := width - sumWordSlice(wordLengths[j:i])
-			if c < 0 {
-				continue
+		runningWidth := -1
+		for j := i - 1; j >= 0; j-- {
+			runningWidth += wordLengths[j] + 1
+			if runningWidth > width {
+				break
 			}
+			c := width - runningWidth
 			c = c*c + cache[j].cost
-			if minCost < 0 || c <= minCost {
+			if minCost < 0 || c < minCost {
 				minCost = c
 				minPrev = j
 			}
@@ -115,7 +114,7 @@ func formatWords(words []string, breaks []int) string {
 
 func charRuler(width int) string {
 	b := bytes.Buffer{}
-	for i := 0; i < lineWrap; i++ {
+	for i := 0; i < width; i++ {
 		b.WriteString(fmt.Sprintf("%d", i%10))
 	}
 	return b.String()
